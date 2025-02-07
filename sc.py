a934cdee2f22ccbe6208cd9de5bc9bba11616025
Destro7ed <joelramoslazaro@gmail.com>
2025-02-07 13:23:54 +0100

    .

--- sc.py
+++ sc.py
@@ -2,3 +2,16 @@
 a=50
 if a%2 == 0:
     print(f"El valor {a} es un numero par")
+else:
+    print(f"El numero {a} es impar")
+
+
+if a>50:
+    print(f"El valor {a} es mayor de 50")
+elif a<50:
+    print(f"El valor {a} es menor de 50")
+else:
+    print(f"El valor {a} es 50")
+
+num= input("Introduce un valor: ")
+print(f"Valor introducido: {num}")
