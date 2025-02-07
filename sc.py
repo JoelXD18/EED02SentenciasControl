201d38fbd0ac4da6ece08ba002f2657c6c6026d7
Destro7ed <joelramoslazaro@gmail.com>
2025-02-07 14:08:11 +0100

    yes

--- sc.py
+++ sc.py
@@ -1,4 +1,5 @@
 #Bifurcacion
+'''
 a=50
 if a%2 == 0:
     print(f"El valor {a} es un numero par")
@@ -14,4 +15,27 @@ else:
     print(f"El valor {a} es 50")
 
 num= input("Introduce un valor: ")
-print(f"Valor introducido: {num}")
+print(f"Valor introducido: {num}") #input siempre devuelve una cadena de  caracteres
+
+num= int(num)
+a=0
+while a<num:
+    print(f"a: {a}")
+    a+=1
+
+    sum = 0
+for a in range(10):
+    sum+=a
+    print(f"(a: {a} sum: {sum})")
+'''
+#bucle que solicite numeros al usuarios hasta que se introduzca un numero par
+result = input("Introduce un numero: ")
+result = int(result)
+
+while result%2 != 0:
+    result = input("Introduce otro numero: ")
+    result = int(result)
+print(f"Valor final: {result}")
+
+
+
